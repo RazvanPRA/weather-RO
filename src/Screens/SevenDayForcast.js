4e7fd8fe8b2992199ae27943ccaadd3b3d67031b
RazvanPRA <prajanu.razvan@gmail.com>
2021-06-07 12:30:59 +0300

    commit -m 'create SevenDayForcast'

--- src/Screens/SevenDayForcast.js
+++ src/Screens/SevenDayForcast.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-const DetailsScreen = () => {
+const SevenDayForcast = () => {
   return (
     <View style={styles.body}>
       <Text>Details Screen</Text>
@@ -9,7 +9,7 @@ const DetailsScreen = () => {
   );
 };
 
-export default DetailsScreen;
+export default SevenDayForcast;
 
 const styles = StyleSheet.create({
   body: {flex: 1, alignItems: 'center', justifyContent: 'center'},
